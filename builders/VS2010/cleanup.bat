07f00e28946a27cc70a6b747861043daf3210330
shiretu <shiretu@01689507-d990-df11-9d7d-0017a411d416>
2010-08-02 18:23:09 +0000

    -- brand new win32 VS2010 project
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@51 01689507-d990-df11-9d7d-0017a411d416

---
+++ builders/VS2010/cleanup.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S _ReSharper.*') DO echo "%%G" && rd /Q /S  "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S *Debug') DO echo "%%G" && rd /Q /S  "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S *Release') DO echo "%%G" && rd /Q /S "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S *ipch') DO echo "%%G" && rd /Q /S  "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /S *.user') DO echo "%%G" && del /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /S *.bak') DO echo "%%G" && del /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /S *.ncb') DO echo "%%G" && del /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /S *.zip') DO echo "%%G" && del /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /S *.xml') DO echo "%%G" && del /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /S *.sdf') DO echo "%%G" && del /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /S /AH *.suo') DO echo "%%G" && del /Q /AH "%%G"
