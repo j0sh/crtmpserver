8fc3cbbc621671bb3541aba1d5a8afb2e0a4e030
shiretu <shiretu@01689507-d990-df11-9d7d-0017a411d416>
2011-01-12 19:05:25 +0000

    -- keep solaris happy
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@264 01689507-d990-df11-9d7d-0017a411d416

--- sources/common/src/platform/solaris/solarisplatform.cpp
+++ sources/common/src/platform/solaris/solarisplatform.cpp
@@ -157,10 +157,6 @@ bool SetFdReuseAddress(int32_t fd) {
 		FATAL("Unable to reuse address");
 		return false;
 	}
-	if (setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, (char *) & one, sizeof (one)) != 0) {
-		FATAL("Unable to reuse port");
-		return false;
-	}
 	return true;
 }
 
