f4f0b1d9d89bf58f3ba8653740d9050f15e98948
jay <jay@01689507-d990-df11-9d7d-0017a411d416>
2011-01-12 13:08:20 +0000

    -- added comments...
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@258 01689507-d990-df11-9d7d-0017a411d416

--- sources/common/include/utils/buffering/iobuffer.h
+++ sources/common/include/utils/buffering/iobuffer.h
@@ -31,7 +31,7 @@
 
 class DLLEXP IOBuffer {
 public:
-	uint8_t *_pBuffer;
+	uint8_t *_pBuffer; //! buffer which holds the data being transmitted/accepted
 	uint32_t _size;
 	uint32_t _published;
 	uint32_t _consumed;
@@ -44,37 +44,121 @@ public:
 	void Initialize(uint32_t expected);
 
 	//Read from a source and put to this buffer
+	/*!
+		@brief Read from Pipe and put to _pBuffer
+	*/
 	bool ReadFromPipe(int32_t fd, uint32_t expected, int32_t &recvAmount);
+
+	/*!
+		@brief Read from TCP File Descriptor and put to _pBuffer. This function is advisable for connection-oriented sockets.
+	*/
 	bool ReadFromTCPFd(int32_t fd, uint32_t expected, int32_t &recvAmount);
+
+	/*!
+		@brief Read from UDP File Descriptor and put to _pBuffer. This function is advisable for connectionless-oriented sockets.
+	*/
 	bool ReadFromUDPFd(int32_t fd, int32_t &recvAmount, sockaddr_in &peerAddress);
+
+	/*!
+		@brief Read from  Standard IO and put to _pBuffer
+	*/
 	bool ReadFromStdio(int32_t fd, uint32_t expected, int32_t &recvAmount);
+
+	/*!
+		@brief Read from File Stream and put to _pBuffer
+	*/
 	bool ReadFromFs(fstream &fs, uint32_t size);
+
+	/*!
+		@brief Read from File Stream and put to _pBuffer using File's 'ReadBuffer' function
+	*/
 	bool ReadFromFs(File &file, uint32_t size);
 #ifdef HAS_MMAP
 	bool ReadFromFs(MmapFile &file, uint32_t size);
 #endif /* HAS_MMAP */
+	/*!
+		@brief Read data from buffer and copy it to _pBuffer
+		@param pBuffer: Buffer that contains the data to be read.
+	*/
 	bool ReadFromBuffer(const uint8_t *pBuffer, const uint32_t size);
+	
+	/*!
+		@brief Read data from an input buffer starting at a designated point
+		@param start: The point where copying starts
+	*/
 	void ReadFromInputBuffer(IOBuffer *pInputBuffer, uint32_t start, uint32_t size);
+
+	/*!
+		@brief Read data from an input buffer
+		@param pBuffer: Buffer that contains the data to be read.
+	*/
 	bool ReadFromInputBuffer(const IOBuffer &buffer, uint32_t size);
+
+	/*!
+		@brief Read data from buffer from a string
+	*/
 	bool ReadFromString(string binary);
+
+	/*!
+		@brief Read data from buffer byte data
+	*/
 	void ReadFromByte(uint8_t byte);
 	bool ReadFromBIO(BIO *pBIO);
 	void ReadFromRepeat(uint8_t byte, uint32_t size);
 
 	//Read from this buffer and put to a destination
+	/*!
+		@brief Read data from buffer and store it.
+		@param sentAmount: Where the data will be stored
+	*/
 	bool WriteToTCPFd(int32_t fd, uint32_t size, int32_t &sentAmount);
+
+	/*!
+		@brief Read data from standard IO and store it.
+	*/
 	bool WriteToStdio(int32_t fd, uint32_t size);
 
 	//Utility functions
+
+	/*!
+		@brief Returns the minimum chunk size. This value is initialized in the constructor.
+	*/
 	uint32_t GetMinChunkSize();
+	/*!
+		@brief Sets the value of the minimum chunk size
+	*/
 	void SetMinChunkSize(uint32_t minChunkSize);
+
+	/*!
+		@brief
+	*/
 	uint32_t GetCurrentWritePosition();
+
+	/*!
+		@brief Returns the _pBuffer pointer
+	*/
 	uint8_t *GetPointer();
 
 	//memory management
+
+	/*!
+		@brief Ignores a specified size of data in the buffer
+	*/
 	bool Ignore(uint32_t size);
+
+	/*!
+		@brief Ifnores all data in the buffer
+	*/
 	bool IgnoreAll();
+
+	/*!
+		@brief Moves the data in a buffer to optimize memory space
+	*/
 	bool MoveData();
+	/*!
+		@brief Makes sure that there is enough memory space in the buffer. If space is not enough, a new buffer is created.
+		@discussion In the case that a new buffer is created and the current buffer has data, the data is copied to the new buffer.
+	*/
 	bool EnsureSize(uint32_t expected);
 
 	//pretty print
