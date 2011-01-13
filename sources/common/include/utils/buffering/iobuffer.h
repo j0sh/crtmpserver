4d4b70bd341b1c793d6fe0305fdd4f0f28c22bd7
jay <jay@01689507-d990-df11-9d7d-0017a411d416>
2011-01-13 08:23:44 +0000

    -- added comments...
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@268 01689507-d990-df11-9d7d-0017a411d416

--- sources/common/include/utils/buffering/iobuffer.h
+++ sources/common/include/utils/buffering/iobuffer.h
@@ -29,9 +29,13 @@
 #define GETAVAILABLEBYTESCOUNT(x)     ((x)._published - (x)._consumed)
 #define GETIBPOINTER(x)     ((uint8_t *)((x)._pBuffer + (x)._consumed))
 
+/*!
+	@class IOBuffer
+	@brief Base class which contains all the necessary functions involving input/output buffering.
+*/
 class DLLEXP IOBuffer {
 public:
-	uint8_t *_pBuffer; //! buffer which holds the data being transmitted/accepted
+	uint8_t *_pBuffer;
 	uint32_t _size;
 	uint32_t _published;
 	uint32_t _consumed;
@@ -41,80 +45,120 @@ public:
 	//constructor/destructor and initialization
 	IOBuffer();
 	virtual ~IOBuffer();
+
+	/*!
+		@brief initializes the buffer
+		@param expected - The expected size of this buffer
+	*/
 	void Initialize(uint32_t expected);
 
 	//Read from a source and put to this buffer
 	/*!
-		@brief Read from Pipe and put to _pBuffer
+		@brief Read from Pipe and saves it.
+		@param fd
+		@param expected - Expected size of the buffer
+		@param recvAmount - Size of data read
 	*/
 	bool ReadFromPipe(int32_t fd, uint32_t expected, int32_t &recvAmount);
 
 	/*!
-		@brief Read from TCP File Descriptor and put to _pBuffer. This function is advisable for connection-oriented sockets.
+		@brief Read from TCP File Descriptor and saves it. This function is advisable for connection-oriented sockets.
+		@param fd - Descriptor that contains the data
+		@param expected - Expected size of the receiving buffer
+		@param recvAmount - Size of data received
 	*/
 	bool ReadFromTCPFd(int32_t fd, uint32_t expected, int32_t &recvAmount);
 
 	/*!
-		@brief Read from UDP File Descriptor and put to _pBuffer. This function is advisable for connectionless-oriented sockets.
+		@brief Read from UDP File Descriptor and saves it. This function is advisable for connectionless-oriented sockets.
+		@param fd - Descriptor that contains the data
+		@param recvAmount - Size of data received
+		@param peerAddress - The source's address
 	*/
 	bool ReadFromUDPFd(int32_t fd, int32_t &recvAmount, sockaddr_in &peerAddress);
 
 	/*!
-		@brief Read from  Standard IO and put to _pBuffer
+		@brief Read from  Standard IO and saves it.
+		@param fd - Descriptor that contains the data
+		@param expected - Expected size of the receiving buffer
+		@param recvAmount - Size of data received
 	*/
 	bool ReadFromStdio(int32_t fd, uint32_t expected, int32_t &recvAmount);
 
 	/*!
-		@brief Read from File Stream and put to _pBuffer
+		@brief Read from File Stream and saves it.
+		@param fs - Descriptor that contains the data
+		@param size - Size of the receiving buffer
 	*/
 	bool ReadFromFs(fstream &fs, uint32_t size);
 
 	/*!
-		@brief Read from File Stream and put to _pBuffer using File's 'ReadBuffer' function
+		@brief Read from File Stream and saves it
+		@param fs - Descriptor that contains the data
+		@param size - Size of the receiving buffer
 	*/
 	bool ReadFromFs(File &file, uint32_t size);
 #ifdef HAS_MMAP
 	bool ReadFromFs(MmapFile &file, uint32_t size);
 #endif /* HAS_MMAP */
 	/*!
-		@brief Read data from buffer and copy it to _pBuffer
-		@param pBuffer: Buffer that contains the data to be read.
+		@brief Read data from buffer and copy it.
+		@param pBuffer - Buffer that contains the data to be read.
+		@param size - Size of data to read.
 	*/
 	bool ReadFromBuffer(const uint8_t *pBuffer, const uint32_t size);
 	
 	/*!
 		@brief Read data from an input buffer starting at a designated point
-		@param start: The point where copying starts
+		@param pInputBuffer - Pointer to the input buffer
+		@param start - The point where copying starts
+		@param size - Size of data to read
 	*/
 	void ReadFromInputBuffer(IOBuffer *pInputBuffer, uint32_t start, uint32_t size);
 
 	/*!
 		@brief Read data from an input buffer
-		@param pBuffer: Buffer that contains the data to be read.
+		@param buffer - Buffer that contains the data to be read.
+		@param size - Size of data to read.
 	*/
 	bool ReadFromInputBuffer(const IOBuffer &buffer, uint32_t size);
 
 	/*!
 		@brief Read data from buffer from a string
+		@param binary - The string that will be read
 	*/
 	bool ReadFromString(string binary);
 
 	/*!
 		@brief Read data from buffer byte data
+		@param byte
 	*/
 	void ReadFromByte(uint8_t byte);
+
+	/*!
+		@brief Read data from a memory BIO
+		@param pBIO
+	*/
 	bool ReadFromBIO(BIO *pBIO);
+
+	/*!
+		@brief
+	*/
 	void ReadFromRepeat(uint8_t byte, uint32_t size);
 
 	//Read from this buffer and put to a destination
 	/*!
 		@brief Read data from buffer and store it.
-		@param sentAmount: Where the data will be stored
+		@param fd
+		@param size - Size of buffer
+		@param sentAmount - Size of data sent
 	*/
 	bool WriteToTCPFd(int32_t fd, uint32_t size, int32_t &sentAmount);
 
 	/*!
 		@brief Read data from standard IO and store it.
+		@param fd
+		@param size
 	*/
 	bool WriteToStdio(int32_t fd, uint32_t size);
 
@@ -126,16 +170,17 @@ public:
 	uint32_t GetMinChunkSize();
 	/*!
 		@brief Sets the value of the minimum chunk size
+		@param minChunkSize
 	*/
 	void SetMinChunkSize(uint32_t minChunkSize);
 
 	/*!
-		@brief
+		@brief Returns the size of the buffer that has already been written on.
 	*/
 	uint32_t GetCurrentWritePosition();
 
 	/*!
-		@brief Returns the _pBuffer pointer
+		@brief Returns the pointer to the buffer
 	*/
 	uint8_t *GetPointer();
 
@@ -143,11 +188,12 @@ public:
 
 	/*!
 		@brief Ignores a specified size of data in the buffer
+		@param size
 	*/
 	bool Ignore(uint32_t size);
 
 	/*!
-		@brief Ifnores all data in the buffer
+		@brief Ignores all data in the buffer
 	*/
 	bool IgnoreAll();
 
@@ -155,13 +201,18 @@ public:
 		@brief Moves the data in a buffer to optimize memory space
 	*/
 	bool MoveData();
+
 	/*!
 		@brief Makes sure that there is enough memory space in the buffer. If space is not enough, a new buffer is created.
+		@param expected - This function makes sure that the size indicated by this parameter is accommodated.
 		@discussion In the case that a new buffer is created and the current buffer has data, the data is copied to the new buffer.
 	*/
 	bool EnsureSize(uint32_t expected);
 
 	//pretty print
+	/*!
+		@brief
+	*/
 	string ToString(uint32_t startIndex = 0, uint32_t limit = 0);
 	operator string();
 
