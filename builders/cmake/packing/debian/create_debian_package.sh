dc6c6054313e6ae5ffc10ed7a283b7ba39009faf
shiretu <shiretu@01689507-d990-df11-9d7d-0017a411d416>
2011-02-21 15:36:31 +0000

    -- work on debian package
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@324 01689507-d990-df11-9d7d-0017a411d416

--- builders/cmake/packing/debian/create_debian_package.sh
+++ builders/cmake/packing/debian/create_debian_package.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
 #create the distribution
-#sh linux.sh
+ORIG=`pwd`
+cd ../
+sh linux.sh
+cd "$ORIG"
 
 #prepare the debian folder
 ROOT=/tmp/crtmpserver
-rm -rf $ROOT
+rm -rf $ROOT*
 mkdir -p $ROOT/DEBIAN
 mkdir -p $ROOT/opt
+mkdir -p $ROOT/etc/init.d
 cp -R /tmp/rtmpserver $ROOT/opt/crtmpserver
-echo "Version: 0.`svnversion -n ../../../sources/`" >$ROOT/DEBIAN/control
+cp crtmpserver $ROOT/etc/init.d/
+echo "Version: 0.`svnversion -n ../../../../sources/`" >$ROOT/DEBIAN/control
 echo "Architecture: `dpkg-architecture|grep DEB_HOST_ARCH=|cut -d "=" -f2`" >>$ROOT/DEBIAN/control
 cat control >>$ROOT/DEBIAN/control
+cp postinst $ROOT/DEBIAN/
+cp postrm $ROOT/DEBIAN/
+cp prerm $ROOT/DEBIAN/
 dpkg --build /tmp/crtmpserver /tmp/
 
