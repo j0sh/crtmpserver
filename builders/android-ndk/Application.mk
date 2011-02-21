72deb4253c47399bcdb784d4ec6d99f29e77ae35
shiretu <shiretu@01689507-d990-df11-9d7d-0017a411d416>
2011-02-21 18:12:15 +0000

    -- removed some old android builders
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@326 01689507-d990-df11-9d7d-0017a411d416

--- builders/android-ndk/Application.mk
+++
@@ -1,4 +0,0 @@
-APP_PROJECT_PATH := $(call my-dir)/project
-APP_MODULES      := lua common
-APP_OPTIM := debug
-
--- builders/android/pack.sh
+++
@@ -1,7 +0,0 @@
-cp ./rtmpserver.lua $3
-cp ./rtmpserver.lua $2
-for i in `find $3/applications -mindepth 1 -maxdepth 1 -type d`;do mkdir -p $i/mediaFolder;done
-tar -pcf ./rtmpd-android.tar.gz $1
-tar -pcf ./rtmpd-android-dynamic.tar.gz $3
-tar -pcf ./rtmpd-android-static.tar.gz $2
-
