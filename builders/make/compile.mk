ad55648d34f800524e476dd5d0d5f9567a4f872e
shiretu <shiretu@01689507-d990-df11-9d7d-0017a411d416>
2011-02-25 08:43:37 +0000

    -- corrected minor search/replace mistake
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@350 01689507-d990-df11-9d7d-0017a411d416

--- builders/make/compile.mk
+++ builders/make/compile.mk
@@ -1,19 +1,19 @@
 #  Copyright (c) 2010,
 #  Gavriloaie Eugen-Andrei (shiretu@gmail.com)
 #
-#  This file is part of ccrtmpserver.
-#  ccrtmpserver is free software: you can redistribute it and/or modify
+#  This file is part of crtmpserver.
+#  crtmpserver is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  ccrtmpserver is distributed in the hope that it will be useful,
+#  crtmpserver is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with ccrtmpserver.  If not, see <http://www.gnu.org/licenses/>.
+#  along with crtmpserver.  If not, see <http://www.gnu.org/licenses/>.
 
 #toolchain paths
 CCOMPILER?=$(TOOLCHAIN_BASE)$(TOOLCHAIN_PREFIX)gcc
