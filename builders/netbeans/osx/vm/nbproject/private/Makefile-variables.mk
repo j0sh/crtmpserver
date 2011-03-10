be30ba03bd51f528c6ef0253aff65980b492f390
shiretu <shiretu@01689507-d990-df11-9d7d-0017a411d416>
2011-03-10 21:29:24 +0000

    -- nb related
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@386 01689507-d990-df11-9d7d-0017a411d416

---
+++ builders/netbeans/osx/vm/nbproject/private/Makefile-variables.mk
@@ -0,0 +1,11 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+# Debug configuration
+CND_SYSINCLUDES_C_Debug=/usr/local/include,/usr/lib/gcc/i686-apple-darwin10/4.2.1/include,/usr/include,/System/Library/Frameworks,/Library/Frameworks
+CND_SYSINCLUDES_CPP_Debug=/usr/include/c++/4.2.1,/usr/include/c++/4.2.1/i686-apple-darwin10/x86_64,/usr/include/c++/4.2.1/backward,/usr/local/include,/usr/lib/gcc/i686-apple-darwin10/4.2.1/include,/usr/include,/System/Library/Frameworks,/Library/Frameworks
+# Release configuration
+CND_SYSINCLUDES_C_Release=/usr/local/include,/usr/lib/gcc/i686-apple-darwin10/4.2.1/include,/usr/include,/System/Library/Frameworks,/Library/Frameworks
+CND_SYSINCLUDES_CPP_Release=/usr/include/c++/4.2.1,/usr/include/c++/4.2.1/i686-apple-darwin10/x86_64,/usr/include/c++/4.2.1/backward,/usr/local/include,/usr/lib/gcc/i686-apple-darwin10/4.2.1/include,/usr/include,/System/Library/Frameworks,/Library/Frameworks
