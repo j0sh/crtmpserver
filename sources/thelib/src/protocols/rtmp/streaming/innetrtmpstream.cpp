37fa6f85b6c5fba7776edf9b8df5f7119fa8a91f
jet <jet@01689507-d990-df11-9d7d-0017a411d416>
2011-04-01 11:51:37 +0000

    -- Fixed incorrect format string
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@417 01689507-d990-df11-9d7d-0017a411d416

--- sources/thelib/src/protocols/rtmp/streaming/innetrtmpstream.cpp
+++ sources/thelib/src/protocols/rtmp/streaming/innetrtmpstream.cpp
@@ -33,7 +33,7 @@ InNetRTMPStream::InNetRTMPStream(BaseProtocol *pProtocol,
 	_rtmpStreamId = rtmpStreamId;
 	_chunkSize = chunkSize;
 	_channelId = channelId;
-	_clientId = format("%d_%d_%z", _pProtocol->GetId(), _rtmpStreamId, (size_t)this);
+	_clientId = format("%d_%d_%zd", _pProtocol->GetId(), _rtmpStreamId, (size_t)this);
 	_lastVideoTime = 0;
 	_lastAudioTime = 0;
 	_pOutFileRTMPFLVStream = NULL;
