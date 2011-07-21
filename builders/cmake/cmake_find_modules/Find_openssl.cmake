6c2dcbf4a34fd57c8f690c120f36635d4bd401f1
shiretu <shiretu@01689507-d990-df11-9d7d-0017a411d416>
2011-07-21 12:39:48 +0000

    -- static compilation of openssl has priority before trying to compile openssl dynamic
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@531 01689507-d990-df11-9d7d-0017a411d416

--- builders/cmake/cmake_find_modules/Find_openssl.cmake
+++ builders/cmake/cmake_find_modules/Find_openssl.cmake
@@ -2,23 +2,24 @@ FIND_PATH(OPENSSL_INCLUDE_PATH
 	NAMES
 		openssl/ssl.h
 	PATHS
+		/opt/local/include
 		/usr/include
 		/usr/local/include
 		/sw/include
-		/opt/local/include
 		/usr/local/ssl/include
 		NO_DEFAULT_PATH)
-
+set(CMAKE_FIND_LIBRARY_SUFFIXES_OLD ${CMAKE_FIND_LIBRARY_SUFFIXES})
+set(CMAKE_FIND_LIBRARY_SUFFIXES .a .so .dylib ${CMAKE_FIND_LIBRARY_SUFFIXES})
 FIND_LIBRARY(OPENSSL_LIBRARY_PATH
 	NAMES
 		ssl
 	PATHS
+		/opt/local/lib
 		/usr/lib64
 		/usr/lib
 		/usr/local/lib64
 		/usr/local/lib
 		/sw/lib
-		/opt/local/lib
 		/usr/local/ssl/lib
 		/lib
 		NO_DEFAULT_PATH)
@@ -27,16 +28,36 @@ FIND_LIBRARY(CRYPTO_LIBRARY_PATH
 	NAMES
 		crypto
 	PATHS
+		/opt/local/lib
 		/usr/lib64
 		/usr/lib
 		/usr/local/lib64
 		/usr/local/lib
 		/sw/lib
-		/opt/local/lib
 		/usr/local/ssl/lib
 		/lib
 		NO_DEFAULT_PATH)
 
+FIND_LIBRARY(Z_LIBRARY_PATH
+    NAMES
+        z
+    PATHS
+        /opt/local/lib
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib64
+        /usr/local/lib
+        /sw/lib
+        /usr/local/ssl/lib
+        /lib
+        NO_DEFAULT_PATH)
+
+set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES_OLD})
+
+#MESSAGE("OPENSSL_INCLUDE_PATH: ${OPENSSL_INCLUDE_PATH}")
+#MESSAGE("OPENSSL_LIBRARY_PATH: ${OPENSSL_LIBRARY_PATH}")
+#MESSAGE("CRYPTO_LIBRARY_PATH: ${CRYPTO_LIBRARY_PATH}")
+#MESSAGE("Z_LIBRARY_PATH: ${Z_LIBRARY_PATH}")
 
 IF(OPENSSL_INCLUDE_PATH)
 	SET(OPENSSL_FOUND 1 CACHE STRING "Set to 1 if openssl is found, 0 otherwise")
@@ -61,6 +82,15 @@ ELSE(CRYPTO_LIBRARY_PATH)
 	SET(OPENSSL_FOUND 0 CACHE STRING "Set to 1 if openssl is found, 0 otherwise")
 	MESSAGE(FATAL_ERROR "Looking for crypto library - not found")
 ENDIF(CRYPTO_LIBRARY_PATH)
+
+
+IF(Z_LIBRARY_PATH)
+    SET(OPENSSL_FOUND 1 CACHE STRING "Set to 1 if openssl is found, 0 otherwise")
+    MESSAGE(STATUS "Looking for z library - found")
+ELSE(Z_LIBRARY_PATH)
+    SET(OPENSSL_FOUND 0 CACHE STRING "Set to 1 if openssl is found, 0 otherwise")
+    MESSAGE(FATAL_ERROR "Looking for z library - not found")
+ENDIF(Z_LIBRARY_PATH)
 
 MARK_AS_ADVANCED(OPENSSL_FOUND)
 
