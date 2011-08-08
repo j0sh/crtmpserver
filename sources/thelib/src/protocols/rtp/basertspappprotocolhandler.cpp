d528f9cec82033bbac204400b05cae260e7d2cb2
shiretu <shiretu@01689507-d990-df11-9d7d-0017a411d416>
2011-08-08 18:19:48 +0000

    -- fix for the last commit
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@565 01689507-d990-df11-9d7d-0017a411d416

--- sources/thelib/src/protocols/rtp/basertspappprotocolhandler.cpp
+++ sources/thelib/src/protocols/rtp/basertspappprotocolhandler.cpp
@@ -994,7 +994,8 @@ OutboundConnectivity *BaseRTSPAppProtocolHandler::GetOutboundConnectivity(
 BaseInNetStream *BaseRTSPAppProtocolHandler::GetInboundStream(string streamName) {
 	//1. get all the inbound network streams which begins with streamName
 	map<uint32_t, BaseStream *> streams = GetApplication()->GetStreamsManager()
-			->FindByTypeByName(ST_IN_NET, streamName, true, false);
+			->FindByTypeByName(ST_IN_NET, streamName, true,
+			GetApplication()->GetAllowDuplicateInboundNetworkStreams());
 	if (streams.size() == 0)
 		return NULL;
 
