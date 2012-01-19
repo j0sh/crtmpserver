cae7cbf074e620ad6958a4bdcab73a5a4224bac0
shiretu <shiretu@01689507-d990-df11-9d7d-0017a411d416>
2012-01-19 14:08:38 +0000

    -- properly implemented HAS_PROTOCOL_RAWHTTPSTREAM guard define -- updated CMakeLists.txt and removed HAS_PROTOCOL_RAWHTTPSTREAM and other unused defines
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@728 01689507-d990-df11-9d7d-0017a411d416

--- sources/thelib/src/streaming/outnetrawstream.cpp
+++ sources/thelib/src/streaming/outnetrawstream.cpp
@@ -1,22 +1,23 @@
-/* 
+/*
  *  Copyright (c) 2010,
  *  Gavriloaie Eugen-Andrei (shiretu@gmail.com)
- *  
+ *
  *  This file is part of crtmpserver.
  *  crtmpserver is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  crtmpserver is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with crtmpserver.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifdef HAS_PROTOCOL_RAWHTTPSTREAM
 #include "streaming/outnetrawstream.h"
 #include "streaming/streamstypes.h"
 #include "protocols/protocoltypes.h"
@@ -96,3 +97,4 @@ bool OutNetRawStream::IsCompatibleWithType(uint64_t type) {
 	//This stream is compatible with everything
 	return true;
 }
+#endif /* HAS_PROTOCOL_RAWHTTPSTREAM */
