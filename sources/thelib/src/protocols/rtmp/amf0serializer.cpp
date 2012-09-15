9aef03ba17caaab993f216edf9b75d6cdda42c8e
shiretu <shiretu@01689507-d990-df11-9d7d-0017a411d416>
2012-09-15 01:41:17 +0000

    -- fixed a critical bug in the AMF0 serializer -- some flex stuff
    git-svn-id: https://svn.rtmpd.com/crtmpserver/trunk@781 01689507-d990-df11-9d7d-0017a411d416

--- sources/thelib/src/protocols/rtmp/amf0serializer.cpp
+++ sources/thelib/src/protocols/rtmp/amf0serializer.cpp
@@ -255,8 +255,8 @@ bool AMF0Serializer::WriteObject(IOBuffer &buffer, Variant &variant,
 	FOR_MAP(temp, string, Variant, i) {
 		string key = MAP_KEY(i);
 		if ((key.length() == 10)
-				&& (key[0] = '0')
-				&& (key[1] = 'x')) {
+				&& (key[0] == '0')
+				&& (key[1] == 'x')) {
 			key = format("%"PRIu32, (uint32_t) strtol(key.c_str(), NULL, 16));
 		}
 		if (!WriteShortString(buffer, key, false)) {
@@ -386,8 +386,8 @@ bool AMF0Serializer::WriteMixedArray(IOBuffer &buffer, Variant &variant,
 	FOR_MAP(temp, string, Variant, i) {
 		string key = MAP_KEY(i);
 		if ((key.length() == 10)
-				&& (key[0] = '0')
-				&& (key[1] = 'x')) {
+				&& (key[0] == '0')
+				&& (key[1] == 'x')) {
 			key = format("%"PRIu32, (uint32_t) strtol(key.c_str(), NULL, 16));
 		}
 		if (!WriteShortString(buffer, key, false)) {
